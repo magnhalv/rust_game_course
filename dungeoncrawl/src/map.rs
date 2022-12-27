d3eb7c693ce39fa294bbb64dc7b6fca88f7b8527
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2022-12-27 15:38:19 +0100

    Add basic graphics with custom font art

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -4,7 +4,8 @@ const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum TileType {
-    Wall, Floor
+    Wall,
+    Floor,
 }
 
 pub struct Map {
@@ -14,22 +15,33 @@ pub struct Map {
 impl Map {
     pub fn new() -> Self {
         Self {
-            tiles: vec![TileType::Floor; NUM_TILES]
+            tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm) {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                let idx = map_idx(x, y);
-                match self.tiles[idx] {
-                    TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'))
-                    },
-                    TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'))
+    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
+        ctx.set_active_console(0);
+        for y in camera.top_y..camera.bottom_y {
+            for x in camera.left_x..camera.right_x {
+                if self.in_bounds(Point::new(x, y)) {
+                    let idx = map_idx(x, y);
+                    match self.tiles[idx] {
+                        TileType::Floor => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('.'),
+                        ),
+                        TileType::Wall => ctx.set(
+                            x - camera.left_x,
+                            y - camera.top_y,
+                            WHITE,
+                            BLACK,
+                            to_cp437('#'),
+                        ),
                     }
-                }                
+                }
             }
         }
     }
@@ -45,13 +57,12 @@ impl Map {
     pub fn try_idx(&self, point: Point) -> Option<usize> {
         if !self.in_bounds(point) {
             None
-        }
-        else {
+        } else {
             Some(map_idx(point.x, point.y))
         }
     }
 }
 
 pub fn map_idx(x: i32, y: i32) -> usize {
-    ((y*SCREEN_WIDTH) + x) as usize
+    ((y * SCREEN_WIDTH) + x) as usize
 }
