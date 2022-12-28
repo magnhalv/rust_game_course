9b253e3bad36fd0a47650f364bb0a46bba9af147
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2022-12-28 13:22:02 +0100

    Add ECS

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -1,36 +1,55 @@
 mod map;
 mod map_builder;
-mod player;
 mod camera;
+mod components;
+mod spawner;
+mod systems;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const SCREEN_WIDTH: i32 = 128;
+    pub const SCREEN_HEIGHT: i32 = 64;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    
     pub use crate::map::*;
-    pub use crate::map_builder::*;
-    pub use crate::player::*;
+    pub use crate::map_builder::*;    
     pub use crate::camera::*;
+    pub use crate::components::*;
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
+
+
+    pub use legion::*;
+    pub use legion::world::SubWorld;
+    pub use legion::systems::CommandBuffer;
 }
 
 use prelude::*;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera: Camera,
+    ecs: World,
+    resources: Resources,
+    systems: Schedule,
 }
 
 impl State {
-    fn new() -> Self {
+    fn new() -> Self {        
+        let mut ecs = World::default();
+        let mut resources = Resources::default();
+        
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+
+        spawn_player(&mut ecs, map_builder.player_start);
+
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
-            map: map_builder.map,
-            player: Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start)
+            ecs,
+            resources,
+            systems: build_scheduler()
+            
         }
     }
 }
@@ -47,9 +66,14 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error");
+        
+        //self.player.update(ctx, &self.map, &mut self.camera);
+        //self.map.render(ctx, &self.camera);
+        //self.player.render(ctx, &self.camera);
     }
 }
 
@@ -57,8 +81,7 @@ fn main() -> BError {
     let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)        
         .with_tile_dimensions(32, 32)            
         .with_resource_path("assets/")
         .with_font("dungeonfont.png", 32, 32)
