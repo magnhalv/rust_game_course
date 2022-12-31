8874143b4644a63fadec050e83e73bd6ea45b1f2
Magnus Halvorsen <magnus.halvorsen@kdi.kongsberg.com>
2022-12-31 13:41:44 +0100

    Add combat system

--- dungeoncrawl/src/systems/player_input.rs
+++ dungeoncrawl/src/systems/player_input.rs
@@ -27,12 +27,29 @@ pub fn player_input(
 
         if delta.x != 0 || delta.y != 0 {
             let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+            let (player_entity, destination) = players.iter(ecs).find_map(|(entity, pos)| Some((*entity, *pos + delta))).unwrap();
 
-            players.iter_mut(ecs).for_each(|(entity, pos)| {
-                let destination = *pos + delta;
-                commands.push(((), WantsToMove{entity: *entity, destination}));          
-                *turn_state = TurnState::PlayerTurn;
-            })
+            let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+            
+            let mut hit_something = false;
+            enemies.iter(ecs).filter(|(_, pos)| { **pos == destination}).for_each(|(entity, _)| {
+                hit_something = true;
+
+                commands.push(((), WantsToAttack {
+                    attacker: player_entity,
+                    victim: *entity,
+                }));
+            });
+
+
+            if !hit_something {
+                commands.push(((), WantsToMove {
+                    entity: player_entity,
+                    destination
+                }));
+            }
+
+            *turn_state = TurnState::PlayerTurn;
         }
     }
 }
